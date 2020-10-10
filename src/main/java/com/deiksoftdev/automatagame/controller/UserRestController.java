57151277dc9d4f402cf243072728edab62ce4111
rkeeves <aprettydecentaddress@gmail.com>
2020-10-10 20:11:49 +0200

    Renamed UserController to UserRestController

--- src/main/java/com/deiksoftdev/automatagame/controller/UserRestController.java
+++ src/main/java/com/deiksoftdev/automatagame/controller/UserRestController.java
@@ -7,22 +7,25 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-
 
 import com.deiksoftdev.automatagame.model.User;
 import com.deiksoftdev.automatagame.repository.UserRepository;
 
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
 @RestController
-@RequestMapping(value = UserController.USER_API)
-public class UserController {
+@RequestMapping(value = UserRestController.USER_API)
+public class UserRestController {
 
 	public static final String USER_API = "/api/users";
 	
 	private final UserRepository userRepository;
+	
+	@Autowired
+	public UserRestController(UserRepository userRepository) {
+		super();
+		this.userRepository = userRepository;
+	}
 
 	@GetMapping("/{userId}")
     public User findById(@PathVariable Long userId) {
