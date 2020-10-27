99110f16556628af8a95e4a26b76159297a18168
rkeeves <aprettydecentaddress@gmail.com>
2020-10-27 11:22:51 +0100

    LandingPage accepts index

--- src/main/java/com/deiksoftdev/automatagame/landing/LandingPageController.java
+++ src/main/java/com/deiksoftdev/automatagame/landing/LandingPageController.java
@@ -1,14 +1,13 @@
 package com.deiksoftdev.automatagame.landing;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LandingPageController {
 
-    @GetMapping("/")
-    public String index(Model model) {
+    @RequestMapping({"/", "/index"})
+    public String index() {
         return "index";
     }
 }
