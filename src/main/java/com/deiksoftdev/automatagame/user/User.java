28f15543dca177c56ef1700974f08ba5558ee9c3
rkeeves <aprettydecentaddress@gmail.com>
2020-10-27 10:47:40 +0100

    Cleaned up placeholders
    Deleted placeholder implementations of all features regarding users.
    Previosuly, just for demo and testing purposes we added a way
    to unsecurely provide CRUD capabilities for User objects.
    Also just for demo purposes we added RestController for User CRUD
    operations, this was deleted too.

--- src/main/java/com/deiksoftdev/automatagame/user/User.java
+++ src/main/java/com/deiksoftdev/automatagame/user/User.java
@@ -1,16 +1,12 @@
-package com.deiksoftdev.automatagame.user;
+package com.deiksoftdev.automatagame.user.model;
 
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.validation.constraints.NotBlank;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -23,10 +19,14 @@ public class User {
 	private long id;
 	
 	@NotBlank(message = "Name is required")
-	@Column(nullable = false)
+	@Column(nullable = false, unique = true)
 	private String name;
 	
 	@NotBlank(message = "Email is required")
-	@Column(nullable = false)
+	@Column(nullable = false, unique = true)
 	private String email;
+
+	@NotBlank(message = "Password is required")
+	@Column(nullable = false)
+	private String password;
 }
--- src/main/java/com/deiksoftdev/automatagame/user/UserRepository.java
+++ src/main/java/com/deiksoftdev/automatagame/user/UserRepository.java
@@ -1,9 +1,12 @@
-package com.deiksoftdev.automatagame.user;
+package com.deiksoftdev.automatagame.user.model;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
-    
+
+    User findByName(String name);
+
+    User findByEmail(String email);
 }
