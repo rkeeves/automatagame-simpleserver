f9214d1c893f9f6bd6e80b0398752f2a79adbd4c
rkeeves <aprettydecentaddress@gmail.com>
2020-11-28 13:17:16 +0100

    resourceserving package

--- src/main/java/com/deiksoftdev/automatagame/config/resourceserving/ResourceServingProperties.java
+++ src/main/java/com/deiksoftdev/automatagame/config/resourceserving/ResourceServingProperties.java
@@ -1,4 +1,4 @@
-package com.deiksoftdev.automatagame.resourceserving;
+package com.deiksoftdev.automatagame.config.resourceserving;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
--- src/main/java/com/deiksoftdev/automatagame/config/resourceserving/StaticResourceConfiguration.java
+++ src/main/java/com/deiksoftdev/automatagame/config/resourceserving/StaticResourceConfiguration.java
@@ -1,4 +1,4 @@
-package com.deiksoftdev.automatagame.resourceserving;
+package com.deiksoftdev.automatagame.config.resourceserving;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
